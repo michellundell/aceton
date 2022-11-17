7003b32105fbd1c544b0bbce0724cf1ee9952c8d
Michel Lundell <michel@lundell.net>
2022-11-17 09:17:05 +0100

    Update sample.c

--- sample.c
+++ sample.c
@@ -1,31 +1,12 @@
 #include <stdio.h>
 #include <string.h>
-
-/* copied from internet academy */
-
-int main(int argc, char **argv)
+void secret(){
+  printf("You have accessed the secret function!!\n");
+}
+int main(int argc, char *argv[])
 {
-    char buff[15];
-    int pass = 0;
-
-    printf("\n Enter the password : \n");
-    gets(buff);
-
-    if(strcmp(buff, "thesecretpassword"))
-    {
-        printf ("\n Wrong Password \n");
-    }
-    else
-    {
-        printf ("\n Correct Password \n");
-        pass = 1;
-    }
-
-    if(pass)
-    {
-       /* Now Give root or admin rights to user*/
-        printf ("\n Root privileges given to the user \n");
-    }
-
-    return(0);
+  char buf[512];
+  strcpy(buf, argv[1]);
+  printf("%s",buf);
+  return(0);
 }
